11aa2da69f7e0cd6a1d177cfc0fb03c1a1e267ed
Maru <meugeniabuono@gmail.com>
2023-02-28 22:15:04 -0300

    Inserción de alerta en e boton enivar

--- js/function.js
+++ js/function.js
@@ -1,15 +1,5 @@
 
 document.getElementById('botón').onclick = function () {
     console.log ("Capturamos el evento click");
-    document.getElementById("demo").innerHTML = "Estamos probando"
-    alert("Suscripcion exitosa")
-}
-
-
-
-
-document.getElementById('botón').onclick = function () {
-    console.log ("Capturamos el evento click");
-    document.getElementById("demo").innerHTML = "Estamos probando"
     alert("Suscripcion exitosa")
 }
